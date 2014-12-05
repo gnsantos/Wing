fc68f32bbe0254275d96d8ff3681049d43c7de3a
Gervasio Santos <gervasio.neto94@gmail.com>
2014-12-05 15:22:53 -0200

    Begining to implement file upload.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -5,7 +5,7 @@
 
 create table Code (
   name                      varchar(255) not null,
-  source                    blob,
+  source                    varbinary(255),
   description_id            integer,
   submitter_id              varchar(255),
   constraint pk_Code primary key (name))
